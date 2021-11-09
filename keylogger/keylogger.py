82e5c2f5af15586108d7125a322a3b7347b24ca3
natalia <ewewraw@gmail.com>
2021-11-09 11:27:06 +0100

    initial commit 2

--- keylogger/keylogger.py
+++ keylogger/keylogger.py
@@ -3,49 +3,83 @@ from pynput import keyboard
 import logging
 import time
 from datetime import datetime
+import sys
 
 data = []
+dynamic_array = []
+line = 1
+logger = None
 
+# The quick brown fox jumps over the lazy dog.
 def on_press(key):
+    global data, dynamic_array
     if key == keyboard.Key.esc:
         return False
-    timestamp = int(round(time.time() * 1000))
-    action = "PRESS"
-    data.append([timestamp, key, action])
+    if key == keyboard.Key.enter:
+        data.append(dynamic_array.copy())
+        dynamic_array.clear()
+    else:
+        timestamp = int(round(time.time() * 1000))
+        action = "PRESS"
+        dynamic_array.append([timestamp, key, action])
+        # print(dynamic_array)
     # logging.info(str(int(round(time.time() * 1000))) + " PRESS " + str(key))
 
+
 def on_release(key):
-    timestamp = int(round(time.time() * 1000))
-    action = "RELEASE"
-    data.append([timestamp, key, action])
+    global line
+    if key == keyboard.Key.enter:
+        print(f"\n{line}", end=" ")
+        line += 1
+    else:
+        timestamp = int(round(time.time() * 1000))
+        action = "RELEASE"
+        dynamic_array.append([timestamp, key, action])
+        # print(dynamic_array)
     # logging.info(str(int(round(time.time() * 1000))) + " RELEASE " + str(key))
 
 
-print("########################################################")
-print("##########      CHOOSE YOUR POKEMON         ############")
-print("########################################################")
-print("# 1 - Alan                                  ############")
-print("# 2 - Natasha                               ############")
-print("# 3 - Joel                                  ############")
-print("# 4 - Giancarlo                             ############")
-print("# 5 - Marcus                                ############")
-print("# 5 - Anonymous hacker                      ############")
-print("""########################################################
-##########   `;-.          ___,         ################   
-##########     `.`\_...._/`.-"`         ################
-##########       \        /      ,          ############
-##########       /()   () \    .' `-._          ########
-##########      |)  .    ()\  /   _.'           ########
-##########      \  -'-     ,; '. <          ############
-##########       ;.__     ,;|   >           ############         
-##########      / ,    / ,  |.-'.-'         ############         
-##########     (_/    (_/ ,;|.<`            ############         
-##########       \    ,     ;-`         ################
-##########        >   \    /            ################
-##########       (_,-'`> .'         ####################         
-##########           (_,'           ####################         
-########################################################""")
+def initLogging(filename):
+    global logger
+    if logger is None:
+        logger = logging.getLogger()
+    else:  # wish there was a logger.close()
+        for handler in logger.handlers[:]:  # make a copy of the list
+            logger.removeHandler(handler)
 
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter(fmt='%(message)s')
+
+    fh = logging.FileHandler(filename)
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+
+    sh = logging.StreamHandler(sys.stdout)
+    sh.setFormatter(formatter)
+    logger.addHandler(sh)
+
+
+print("⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿")
+print("⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿      CHOOSE YOUR POKEMON         ⣿⣿⣿⣿⣿⣿⣿")
+print("⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿")
+print("⣿ 1 - Alan                                  ⣿⣿⣿⣿⣿⣿⣿⣿⣿")
+print("⣿ 2 - Natasha                               ⣿⣿⣿⣿⣿⣿⣿⣿⣿")
+print("⣿ 3 - Joel                                  ⣿⣿⣿⣿⣿⣿⣿⣿⣿")
+print("⣿ 4 - Giancarlo                             ⣿⣿⣿⣿⣿⣿⣿⣿⣿")
+print("⣿ 5 - Marcus                                ⣿⣿⣿⣿⣿⣿⣿⣿⣿")
+print(" 5 - Russian or Chinese hacker             ⣿⣿⣿⣿⣿⣿⣿⣿⣿")
+print("""⣿⣿⣿⣿⣿⡏⠉⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⠀⠀⠀⠈⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⠉⠁⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣧⡀⠀⠀⠀⠀⠙⠿⠿⠿⠻⠿⠿⠟⠿⠛⠉⠀⠀⠀⠀⠀⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣷⣄⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⡟⠀⠀⢰⣹⡆⠀⠀⠀⠀⠀⠀⣭⣷⠀⠀⠀⠸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠈⠉⠀⠀⠤⠄⠀⠀⠀⠉⠁⠀⠀⠀⠀⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⢾⣿⣷⠀⠀⠀⠀⡠⠤⢄⠀⠀⠀⠠⣿⣿⣷⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⡀⠉⠀⠀⠀⠀⠀⢄⠀⢀⠀⠀⠀⠀⠉⠉⠁⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿""")
+print()
 
 nr = input()
 
@@ -53,16 +87,18 @@ pokemons = ["Alan", "Natasha", "Joel", "Giancarlo", "Marcus", "Russian_or_Chines
 
 pokemon = pokemons[int(nr) - 1]
 log_dir = "CollectedData/" + pokemon + "/"
-log_timestamp = datetime.now().strftime("%d-%m-%Y_%Hh%Mm%Ss")
-logging.basicConfig(filename=(log_dir + log_timestamp + ".csv"), level=logging.DEBUG, format='%(message)s')
 
 
-with Listener(on_press = on_press, on_release = on_release) as listener:
+with Listener(on_press=on_press, on_release=on_release) as listener:
     listener.join()
 
-start_timestamp = data[0][0]
-
-for item in data:
-    item[0] -= start_timestamp
-    # log = [str(item[0]), str(item[1]), item[2]]
-    logging.info(str(item[0]) + ', ' + str(item[1]) + ', ' + item[2])
+i = 0
+for array in data:
+    log_timestamp = datetime.now().strftime("%d-%m-%Y_%Hh%Mm%Ss")
+    initLogging(log_dir + log_timestamp + str(i) + ".csv")
+    i += 1
+    start_timestamp = array[0][0]
+    for item in array:
+        item[0] -= start_timestamp
+        # log = [str(item[0]), str(item[1]), item[2]]
+        logging.info(str(item[0]) + ', ' + str(item[1]) + ', ' + item[2])
